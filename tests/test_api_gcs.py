24773ad81a43e2f55bc2401678fe60416cac7088
Adrian <awrobel.wr@gmail.com>
2025-07-15 19:28:27 +0200

    Updated tests. Completed writting tests for fetch_data and normalize_data functions.

--- tests/test_api_gcs.py
+++ tests/test_api_gcs.py
@@ -1,14 +1,12 @@
 """Testing api_gcs script by using pytest"""
-import logging
 
 # Importing modules.
 import pytest
 from unittest.mock import patch, MagicMock
-
 import requests.exceptions
-
 from openaq_data_pipeline.api_gcs import fetch_data, normalize_data, save_to_file, run
-from requests.exceptions import HTTPError
+import pandas as pd
+
 
 
 # === Testing fetch_data ===
@@ -99,3 +97,139 @@ def test_fetch_data_empty(mock_log_warning, mock_get):
 
     assert data is None
     mock_log_warning.assert_called_once_with(f"Empty response from {url}")
+
+
+
+# === Testing normalize_data ===
+
+# Correct DataFrame.
+def test_normalize_data_success():
+    json_data = {
+        "results" : [
+            {
+                "parameter.name" : "pm25",
+                "latest.value" : 12,
+                "latest.datetime.utc" : "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "pm10",
+                "latest.value": 20,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "o3",
+                "latest.value": 17,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "no2",
+                "latest.value": 22,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            }
+        ]
+    }
+    city = "test_city"
+    location = "test_location"
+
+    df = normalize_data(json_data, city, location)
+
+    assert isinstance(df, pd.DataFrame)
+    assert {"city", "location", "date", "time", "pm10", "pm25", "no2", "o3"}.issubset(df.columns)
+    assert df["city"].iloc[0] == city
+    assert df["location"].iloc[0] == location
+
+
+# Empty DataFrame.
+def test_normalize_data_empty_df():
+    json_data = {"results" : []}
+    result = normalize_data(json_data, "city", "location")
+    assert result is None
+
+
+# Df is not a list.
+def test_normalize_data_not_list():
+    json_data = {"results" : "mot a list"}
+    result = normalize_data(json_data, "city", "location")
+    assert result is None
+
+
+# Df has no results.
+def test_normalize_data_no_results():
+    json_data = {}
+    result = normalize_data(json_data, "city", "location")
+    assert result is None
+
+
+# Df has no required params.
+def test_normalize_data_no_req_params():
+    json_data = {
+        "results" : [
+            {
+                "parameter.name" : "co2",
+                "latest.value": 13,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            }
+        ]
+    }
+
+    df = normalize_data(json_data, "city", "location")
+
+    assert isinstance(df, pd.DataFrame)
+    assert df.empty
+
+
+# Date and time are correctly converted.
+def test_normalize_data_date_time_format():
+    json_data = {
+        "results" : [
+            {
+                "parameter.name" : "pm25",
+                "latest.value": 13,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            }
+        ]
+    }
+
+    df = normalize_data(json_data, "city", "location")
+
+    assert df["date"].iloc[0] == "15:07:2025"
+    assert df["time"].iloc[0] == "12:00:00"
+
+
+# Checking pivoting.
+def test_normalize_data_pivot():
+    json_data = {
+        "results" : [
+            {
+                "parameter.name" : "pm25",
+                "latest.value": 13,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "pm10",
+                "latest.value": 7,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "o3",
+                "latest.value": 17,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            },
+            {
+                "parameter.name": "no2",
+                "latest.value": 8,
+                "latest.datetime.utc": "2025-07-15T12:00:00Z"
+            }
+        ]
+    }
+
+    df = normalize_data(json_data, "city", "location")
+
+    assert "pm25" in df.columns
+    assert "pm10" in df.columns
+    assert "o3" in df.columns
+    assert "no2" in df.columns
+    assert df["pm25"].iloc[0] == 13
+    assert df["pm10"].iloc[0] == 7
+    assert df["o3"].iloc[0] == 17
+    assert df["no2"].iloc[0] == 8
